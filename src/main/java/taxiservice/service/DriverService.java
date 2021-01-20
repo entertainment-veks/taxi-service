8ff4b296d32c29e6166aaac9aa26c57ebaeed730
egorka <entertainment.veks@gmail.com>
2021-01-20 11:43:07 +0200

    code refactored following Maxim's recomendations

--- src/main/java/taxiservice/service/DriverService.java
+++ src/main/java/taxiservice/service/DriverService.java
@@ -1,6 +1,5 @@
 package taxiservice.service;
 
-import java.util.List;
 import taxiservice.model.Driver;
 
 public interface DriverService extends GenericService<Driver, Long> {
--- src/main/java/taxiservice/service/ManufacturerService.java
+++ src/main/java/taxiservice/service/ManufacturerService.java
@@ -1,6 +1,5 @@
 package taxiservice.service;
 
-import java.util.List;
 import taxiservice.model.Manufacturer;
 
 public interface ManufacturerService extends GenericService<Manufacturer, Long> {
